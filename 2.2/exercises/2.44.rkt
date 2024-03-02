54cbf47a9502419d3c2a4a469a56aa48d4042642
Ravachol <ravachol.yang@gmail.com>
2024-03-02 14:03:14 +0800

    ex2.44

---
+++ 2.2/exercises/2.44.rkt
@@ -0,0 +1,39 @@
+#lang sicp
+;; Exercise 2.44: Define the procedure up-split used by corner-split.
+;; It is similar to right-split, except that it switches the roles of below and beside.
+
+(#%require sicp-pict)
+
+;; right split
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter 
+                                  (- n 1))))
+        (beside painter 
+                (below smaller smaller)))))
+
+;; up split
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter
+                               (- n 1))))
+        (below (beside smaller smaller)
+               painter))))
+
+;; corner split
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter 
+                                (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right 
+                                   right))
+              (corner (corner-split painter 
+                                    (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right 
+                         corner))))))
