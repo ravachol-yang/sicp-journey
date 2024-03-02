3e022a9af5fb93c280367cfd56fa47fd5ee5acd6
Ravachol <ravachol.yang@gmail.com>
2024-03-02 17:53:42 +0800

    ex2.48 Updated

--- 2.2/exercises/2.48.rkt
+++ 2.2/exercises/2.48.rkt
@@ -5,32 +5,6 @@
 ;; Use your vector representation from Exercise 2.46 to define a representation
 ;; for segments with a constructor make-segment and selectors start-segment and end-segment.
 
-(define (make-vect x y)
-  (cons x y))
-
-(define (xcor-vect v)
-  (car v))
-
-(define (ycor-vect v)
-  (cdr v))
-
-(define (2vect v1 v2 op)
-  (make-vect (op (xcor-vect v1)
-                 (xcor-vect v2))
-             (op (ycor-vect v1)
-                 (ycor-vect v2))))
-
-(define (add-vect v1 v2)
-  (2vect v1 v2 +))
-
-(define (sub-vect v1 v2)
-  (2vect v1 v2 -))
-
-(define (scale-vect v scalar)
-  (make-vect (* scalar (xcor-vect v))
-             (* scalar (ycor-vect v))))
-
-
 (define (make-segment v1 v2)
   (cons v1 v2))
 
@@ -38,5 +12,4 @@
   (car s))
 
 (define (end-segment s)
-  (add-vect (car s)
-            (cdr s)))
+  (cdr s))
