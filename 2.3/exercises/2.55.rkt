a13f698d4593a161b3ec8defb301fe2d20fce9ce
Ravachol <ravachol.yang@gmail.com>
2024-03-03 20:01:33 +0800

    ex2.55

---
+++ 2.3/exercises/2.55.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+;; Exercise 2.55: Eva Lu Ator types to the interpreter the expression
+
+(car ''abracadabra)
+
+;; To her surprise, the interpreter prints back quote. Explain.
+
+;; =
+(car (quote (quote abracadabra)))
