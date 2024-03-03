4b88c783a5046a3e02743acc13b0d885c1c0cebb
Ravachol <ravachol.yang@gmail.com>
2024-03-03 18:41:35 +0800

    ex2.50

---
+++ 2.2/exercises/2.50.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+;; Exercise 2.50: Define the transformation flip-horiz, which flips painters horizontally,
+;; and transformations that rotate painters counterclockwise by 180 degrees and 270 degrees.
+
+(#%require sicp-pict)
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
