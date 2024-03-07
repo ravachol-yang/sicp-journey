f939e849c9db259712ff2c06555caedd8630df0e
Ravachol <ravachol.yang@gmail.com>
2024-03-07 21:31:37 +0800

    ex2.65

---
+++ 2.3/exercises/2.65.rkt
@@ -0,0 +1,78 @@
+#lang sicp
+;; Exercise 2.65: Use the results of Exercise 2.63 and Exercise 2.64 to give Theta ( n ) implementations
+;; of union-set and intersection-set for sets implemented as (balanced) binary trees.107
+
+;; Tree operations
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (list->tree elements)
+  (car (partial-tree 
+        elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size 
+             (quotient (- n 1) 2)))
+        (let ((left-result 
+               (partial-tree 
+                elts left-size)))
+          (let ((left-tree 
+                 (car left-result))
+                (non-left-elts 
+                 (cdr left-result))
+                (right-size 
+                 (- n (+ left-size 1))))
+            (let ((this-entry 
+                   (car non-left-elts))
+                  (right-result 
+                   (partial-tree 
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree 
+                     (car right-result))
+                    (remaining-elts 
+                     (cdr right-result)))
+                (cons (make-tree this-entry 
+                                 left-tree 
+                                 right-tree)
+                      remaining-elts))))))))
+
+(define (tree->list tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list 
+         (left-branch tree)
+         (cons (entry tree)
+               (copy-to-list 
+                (right-branch tree)
+                result-list)))))
+  (copy-to-list tree '()))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1))
+                    (x2 (car set2)))
+                (cond ((< x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        set2)))
+                      ((= x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        (cdr set2))))
+                      (else
+                       (cons x2
+                             (union-set set1
+                                        (cdr set2)))))))))
+
+;; Tests
+(union-set '(1 2 3 6) '(1 3 5 6 7))
+
+
